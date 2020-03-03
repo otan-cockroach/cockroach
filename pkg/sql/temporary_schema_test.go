7a04dc1d976c5a0d725684edbdc1c409989a0537
Oliver Tan <otan@cockroachlabs.com>
2020-03-03 11:11:45 -0800

    sql: implement a background task for temp object cleanup
    This PR introduces a `TempObjectCleaner`, which is a background task
    that runs every 30mins on every node, but will only be executed by the
    node that has the meta1 leaseholder. This task will cleanup dangling
    temporary schemas that could not have been cleaned up by the connection.
    The job also comes with logging and metrics. A simple test mocking the
    ticking mechanism has also been included.
    
    Also refactored the existing temporary schema deletion to use ExecEx,
    which overrides the SearchPath / User executing the deletion at a lower
    layer rather than using `SetSessionExecutor`.
    
    Release note (sql change): Introduced a temporary table cleanup job that
    runs once every 30mins per cluster that will remove any temporary
    schemas and their related objects that did not get removed cleanly when
    the connection closed.

--- pkg/sql/temporary_schema_test.go
+++ pkg/sql/temporary_schema_test.go
@@ -8,7 +8,7 @@
 // by the Apache License, Version 2.0, included in the file
 // licenses/APL.txt.
 
-package sql_test
+package sql
 
 import (
 	"context"
@@ -16,14 +16,19 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/cockroachdb/cockroach/pkg/base"
 	"github.com/cockroachdb/cockroach/pkg/internal/client"
-	"github.com/cockroachdb/cockroach/pkg/sql"
 	"github.com/cockroachdb/cockroach/pkg/sql/sessiondata"
 	"github.com/cockroachdb/cockroach/pkg/sql/sqlbase"
 	"github.com/cockroachdb/cockroach/pkg/sql/tests"
+	"github.com/cockroachdb/cockroach/pkg/testutils"
 	"github.com/cockroachdb/cockroach/pkg/testutils/serverutils"
+	"github.com/cockroachdb/cockroach/pkg/testutils/sqlutils"
 	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/timeutil"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -90,15 +95,10 @@ INSERT INTO perm_table VALUES (DEFAULT, 1);
 	require.NoError(
 		t,
 		kvDB.Txn(ctx, func(ctx context.Context, txn *client.Txn) error {
-			err = sql.TestingCleanupSchemaObjects(
+			err = cleanupSchemaObjects(
 				ctx,
-				s.ExecutorConfig().(sql.ExecutorConfig).Settings,
-				func(
-					ctx context.Context, _ string, _ *client.Txn, query string, _ ...interface{},
-				) (int, error) {
-					_, err := conn.ExecContext(ctx, query)
-					return 0, err
-				},
+				s.ExecutorConfig().(ExecutorConfig).Settings,
+				s.InternalExecutor().(*InternalExecutor),
 				txn,
 				namesToID["defaultdb"],
 				schemaName,
@@ -126,4 +126,103 @@ INSERT INTO perm_table VALUES (DEFAULT, 1);
 	).Scan(&colDefault)
 	require.NoError(t, err)
 	assert.False(t, colDefault.Valid)
+}
+
+func TestTemporaryObjectCleanerRetry(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	sec := time.Second
+	testCases := []struct {
+		errTimes       int
+		expectedSleeps []time.Duration
+		expectedErr    bool
+	}{
+		{0, []time.Duration{}, false},
+		{1, []time.Duration{sec}, false},
+		{2, []time.Duration{sec, sec * 2}, false},
+		{3, []time.Duration{sec, sec * 2, sec * 4}, false},
+		{4, []time.Duration{sec, sec * 2, sec * 4, sec * 8}, false},
+		{5, []time.Duration{sec, sec * 2, sec * 4, sec * 8}, true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("errTimes: %d", tc.errTimes), func(t *testing.T) {
+			sleeps := []time.Duration{}
+			cleaner := &TemporaryObjectCleaner{
+				sleepFunc: func(d time.Duration) {
+					sleeps = append(sleeps, d)
+				},
+			}
+			i := 0
+			err := cleaner.retry(
+				context.Background(),
+				func() error {
+					if i == tc.errTimes {
+						return nil
+					}
+					i++
+					return fmt.Errorf("wooper")
+				},
+			)
+			if tc.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tc.expectedSleeps, sleeps)
+		})
+	}
+}
+
+func TestTemporaryObjectCleaner(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+
+	ch := make(chan time.Time)
+	knobs := base.TestingKnobs{
+		SQLExecutor: &ExecutorTestingKnobs{
+			DisableTempObjectsCleanupOnSessionExit: true,
+			TempObjectsCleanupCh:                   ch,
+		},
+	}
+	tc := serverutils.StartTestCluster(t, 2, /* numNodes */
+		base.TestClusterArgs{
+			ServerArgs: base.TestServerArgs{
+				UseDatabase: "defaultdb",
+				Knobs:       knobs,
+			},
+		})
+	defer tc.Stopper().Stop(context.TODO())
+
+	db := tc.ServerConn(0)
+	sqlDB := sqlutils.MakeSQLRunner(db)
+	sqlDB.Exec(t, `SET experimental_enable_temp_tables=true`)
+	sqlDB.Exec(t, `CREATE TEMP TABLE t (x INT)`)
+
+	// Close the client connection. Normally the temporary data would immediately
+	// be cleaned up on session exit, but this is disabled via the
+	// DisableTempObjectsCleanupOnSessionExit testing knob.
+	if err := db.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Sanity check: there should still be one temporary schema present.
+	db = tc.ServerConn(1)
+	sqlDB = sqlutils.MakeSQLRunner(db)
+	tempSchemaQuery := `SELECT count(*) FROM system.namespace WHERE name LIKE 'pg_temp%'`
+	var tempSchemaCount int
+	sqlDB.QueryRow(t, tempSchemaQuery).Scan(&tempSchemaCount)
+	require.Equal(t, tempSchemaCount, 1)
+
+	// Now force a cleanup run (by default, it is every 30mins).
+	ch <- timeutil.Now()
+
+	// Verify that the asynchronous cleanup job kicks in and removes the temporary
+	// data.
+	testutils.SucceedsSoon(t, func() error {
+		sqlDB.QueryRow(t, tempSchemaQuery).Scan(&tempSchemaCount)
+		if tempSchemaCount > 0 {
+			return errors.Errorf("expected 0 temp schemas, found %d", tempSchemaCount)
+		}
+		return nil
+	})
 }
